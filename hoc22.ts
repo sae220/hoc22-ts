ab84f37eb5a4f690da68a85120eb5d6e7689f747
neonerz <gerald@rewrite.media>
2022-04-18 23:16:59 -0400

    Update hoc22.ts

--- hoc22.ts
+++ hoc22.ts
@@ -8,25 +8,43 @@ enum Weight {
     COW
 }
 
+enum Direction {
+    //% block="forward"
+    Forward,
+    //% block="back"
+    Back,
+    //% block="left"
+    Left,
+    //% block="right"
+    Right
+}
+
 enum ColoredBlocks {
-    //% blockIdentity="blocks.block" enumval=262380 block="Yellow Concrete"
+    //% blockIdentity="blocks.block" enumval=262380 block="yellow"
     //% jres alias=YELLOW_CONCRETE
     YellowConcrete = 262380,
-    //% blockIdentity="blocks.block" enumval=131308 block="Magenta Concrete"
+    //% blockIdentity="blocks.block" enumval=131308 block="magenta"
     //% jres alias=MAGENTA_CONCRETE
     MagentaConcrete = 131308,
-    //% blockIdentity="blocks.block" enumval=196844 block="Light Blue Concrete"
+    //% blockIdentity="blocks.block" enumval=196844 block="light blue"
     //% jres alias=LIGHT_BLUE_CONCRETE
     LightBlueConcrete = 196844,
-    //% blockIdentity="blocks.block" enumval=327916 block="Lime Concrete"
+    //% blockIdentity="blocks.block" enumval=327916 block="lime"
     //% jres alias=LIME_CONCRETE
     LimeConcrete = 327916
-    
+
 }
 
 // global variables
 const blockDropPosition = world(0, 0, 0)
 const weightDropPosition = world(27, 27, 33)
+
+const directions = [
+    FORWARD,
+    BACK,
+    LEFT,
+    RIGHT
+];
 
 //%  block="Hour of Code 2022" weight=200 color=#6100ff icon="\u26EB"
 namespace hoc22 {
@@ -34,7 +52,7 @@ namespace hoc22 {
     /**
      * Summon colored blocks (POC1-a)
      */
-    //% block="Drop %b block"
+    //% block="drop %b block"
     export function summonColoredBlock(b: ColoredBlocks): void {
 
         blocks.place(b, blockDropPosition);
@@ -52,5 +70,13 @@ namespace hoc22 {
 
     }
 
+    /**
+     * Agent Move (POC1-c)
+     */
+    //% block="agent move %d by %n"
+    export function agentMove(d: Direction, n: number): void {
+        const direction = directions[d];
+        agent.move(direction, n)
 
+    }
 }
